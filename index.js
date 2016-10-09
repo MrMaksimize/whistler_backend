00f1fb42bd2947ab5041560b804da35c6009e2d0
Maksim Pecherskiy <max@maksimize.com>
2016-10-09 01:35:53 -0700

    Finish testing particle.

--- index.js
+++ index.js
@@ -3,6 +3,35 @@
 const AWS = require('aws-sdk');
 require("./environment.js");
 var twilio = require('twilio');
+var querystring = require('querystring');
+var moment = require('moment');
+var receiverNumbers = require('./recipients.json');
+
+
+var sendMessages = function(eventInfo) {
+    var twilio_account_sid = process.env.TWILIO_ACCOUNT_SID;
+    var twilio_auth_token  = process.env.TWILIO_AUTH_TOKEN;
+    var twilio_num    = process.env.MY_PHONE_NUMBER;
+    var client             = new twilio.RestClient(twilio_account_sid, twilio_auth_token);
+    console.log(receiverNumbers);
+    for (var i = 0; i < receiverNumbers.length; i++) {
+        console.log("Send to " + receiverNumbers[i]);
+        console.log(eventInfo.body);
+        console.log(eventInfo.mediaURL);
+        client.messages.create({
+            body: eventInfo.body,
+            to: receiverNumbers[i],  // Text this number
+            from: twilio_num, // From a valid Twilio number
+            mediaUrl: eventInfo.mediaURL
+        }, function(err, message) {
+            if(err) {
+                console.error(err.message);
+            }
+        });
+    }
+}
+
+
 
 /**
  * The following JSON template shows what is sent as the payload:
@@ -19,47 +48,36 @@ var twilio = require('twilio');
  * http://docs.aws.amazon.com/iot/latest/developerguide/iot-lambda-rule.html
  */
 exports.handler = function(event, context) {
-  console.log(event);
-  var default_receiver = "+17736777755"
-  var receiverNumbers = [
-      "+18587171440",
-      "+17736777755"
-  ]
-  var twilio_account_sid = process.env.TWILIO_ACCOUNT_SID;
-  var twilio_auth_token  = process.env.TWILIO_AUTH_TOKEN;
-  var twilio_num    = process.env.MY_PHONE_NUMBER;
-  var client             = new twilio.RestClient(twilio_account_sid, twilio_auth_token);
-  var body = ''
-  var mediaURL = ''
-  var singleMedia = 'https://api.mapbox.com/v4/mapbox.streets-satellite/-116.999824,33.094,17/300x300@2x.png?access_token=pk.eyJ1IjoibXJtYWtzaW1pemUiLCJhIjoiRlRwLWwyVSJ9.--Y4RdEJ_5ZuJjSUkx34vQ'
-  var doubleMedia = 'https://api.mapbox.com/v4/mapbox.streets-satellite/pin-l-danger+ff0000(-116.999824,33.094),pin-l-danger+ff0000(-116.999824,33.095)/-116.9999,33.094,17/300x300@2x.png?access_token=pk.eyJ1IjoibXJtYWtzaW1pemUiLCJhIjoiRlRwLWwyVSJ9.--Y4RdEJ_5ZuJjSUkx34vQ'
-  var longMedia = 'https://api.mapbox.com/v4/mapbox.streets-satellite/pin-l-danger+ff0000(-116.9999,33.094),pin-l-danger+ff0000(-116.9999,33.095),pin-l-danger+ff0000(-116.9999,33.093),pin-l-danger+ff0000(-116.9992,33.094),pin-l-danger+ff0000(-116.9989,33.094)/-116.9999,33.094,17/300x300@2x.png?access_token=pk.eyJ1IjoibXJtYWtzaW1pemUiLCJhIjoiRlRwLWwyVSJ9.--Y4RdEJ_5ZuJjSUkx34vQ'
 
-  switch(event.clickType) {
-      case "SINGLE":
-        body = "Poacher reported in Escondido area. Respond immediately";
-        mediaURL = singleMedia;
-        break;
+  var eventTime = moment().format("MMM Do YYYY");
+  var eventInfo = {
+    body: '[' + eventTime + '] Poacher reported in area. Respond Immediately. '
+  }
+
+
+  if (event.serialNumber) {
+      eventInfo.deviceName = event.serialNumber;
+      eventInfo.latlng = "-116.999824,33.094";
+      eventInfo.mediaURL = "https://api.mapbox.com/v4/mapbox.streets-satellite/" + eventInfo.latlng + ",17/300x300@2x.png?access_token=pk.eyJ1IjoibXJtYWtzaW1pemUiLCJhIjoiRlRwLWwyVSJ9.--Y4RdEJ_5ZuJjSUkx34vQi";
+
+    switch(event.clickType) {
       case "DOUBLE":
-        body = "Poachers moving in the area. Respond Immediately";
-        mediaURL = doubleMedia;
+        eventInfo.body += "Multiple units detected moving north.";
+        eventInfo.mediaURL = 'https://api.mapbox.com/v4/mapbox.streets-satellite/pin-l-danger+ff0000(-116.999824,33.094),pin-l-danger+ff0000(-116.999824,33.095)/' + eventInfo.latlng + ',17/300x300@2x.png?access_token=pk.eyJ1IjoibXJtYWtzaW1pemUiLCJhIjoiRlRwLWwyVSJ9.--Y4RdEJ_5ZuJjSUkx34vQ';
         break;
       case "LONG":
-        body = "Multiple armed units detected. Notified UN Peacekeepers and local military forces."
-        mediaURL = longMedia;
+        eventInfo.body += "Multiple armed units. Notified UN Peacekeepers and local military forces."
+        eventInfo.mediaURL = 'https://api.mapbox.com/v4/mapbox.streets-satellite/pin-l-danger+ff0000(-116.9999,33.094),pin-l-danger+ff0000(-116.9999,33.095),pin-l-danger+ff0000(-116.9999,33.093),pin-l-danger+ff0000(-116.9992,33.094),pin-l-danger+ff0000(-116.9989,33.094)/'+ eventInfo.latlng +',17/300x300@2x.png?access_token=pk.eyJ1IjoibXJtYWtzaW1pemUiLCJhIjoiRlRwLWwyVSJ9.--Y4RdEJ_5ZuJjSUkx34vQ'
         break;
+    }
+  }
+  // Particle
+  else {
+      eventInfo.deviceName = querystring.parse(event.body.data);
+      eventInfo.latlng = "-116.999824,33.094";
+      eventInfo.mediaURL = 'https://api.mapbox.com/v4/mapbox.streets-satellite/pin-l-danger+ff0000(-116.999824,33.094)/'+ eventInfo.latlng +',17/300x300@2x.png?access_token=pk.eyJ1IjoibXJtYWtzaW1pemUiLCJhIjoiRlRwLWwyVSJ9.--Y4RdEJ_5ZuJjSUkx34vQ'
   }
 
-  for (var i = 0; i < receiverNumbers.length; i++) {
-      client.messages.create({
-          body: body,
-          to: receiverNumbers[i],  // Text this number
-          from: twilio_num, // From a valid Twilio number
-          mediaUrl: mediaURL
-      }, function(err, message) {
-          if(err) {
-              console.error(err.message);
-          }
-      });
-  }
+  sendMessages(eventInfo)
+
 };
